695084eab3b0920366f9833940c285e68a0c5c8f
sosyan6 <rmario1188@yahoo.co.jp>
2021-11-26 10:52:27 +0900

    Add files via upload

--- UmamusumeLauncher.ps1
+++ UmamusumeLauncher.ps1
@@ -7,10 +7,10 @@ Param(
 Add-Type -MemberDefinition @'
 public struct RECT
 {
-    public int left;
-    public int top;
-    public int right;
-    public int bottom;
+	public int left;
+	public int top;
+	public int right;
+	public int bottom;
 }
 
 [DllImport("user32.dll")]
